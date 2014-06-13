dc54541d99d9db153440d976cab18a7bf7c98dee
davin@wordpainter.net <davin@wordpainter.net@ea13b45b-4320-831c-2a51-b47400ebaf46>
2014-06-13 01:47:55 +0000

    Recycling nop-unidle branch.

--- branches/nop-unidle/src/inc/version.h
+++
@@ -1,7 +0,0 @@
-#define SVNVER "38"
-#define PROTOBASE "2.1-r" SVNVER
-#define PROTOVER "Proto" PROTOBASE
-#define NEONVER "Neon2.17"
-#define VERSION "Muck2.2fb6.91"
-
-extern const char *infotext[];
